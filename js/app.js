dcf498ba61a6cbd89afd36f1a757ff962d7fe59e
octavio pezoa <octaviopezoa@MacBook-Pro-de-octavio.local>
2022-04-17 15:06:10 -0400

    mostramos presupuesto en pantalla

--- js/app.js
+++ js/app.js
@@ -6,22 +6,46 @@ const gastoListado = document.querySelector('#gastos-ul');
 // eventos
 eventListeners();
 function eventListeners() {
-    document.addEventListener('DOMContentLoader', preguntarPresupuesto);
+    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
 }
 
 
 // clases
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
 
+class UI {
+    insertarPresupuesto(cantidad) {
+        // extraemos valor
+        const { presupuesto, restante } = cantidad;
 
+        // agregamos al html
+        document.querySelector('#total').textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+    }
+};
+
+// instanciamos las clases
+const ui = new UI();
+let presupuesto; //variable global
 
 
 // funciones
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿cuañ es tu presupuesto?');
 
-    console.log(Number(presupuestoUsuario));
-
     if (presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
         window.location.reload(); //recargamos si la condicion es falsa;
     }
+
+    // presupuesto valido
+    presupuesto = new Presupuesto(presupuestoUsuario);
+
+    // mostramos presupuesto en el html
+    ui.insertarPresupuesto(presupuesto);
 }
